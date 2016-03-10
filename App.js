dccfa699a1838c58abfc764670afc96caf8cf68b
Ganesh Umashankar <ganirockz@gmail.com>
2016-03-10 10:32:14 +0530

    state ref etc

--- App.js
+++ App.js
@@ -1,9 +1,51 @@
 import React from 'react';
-class App extends React.Component  {
-  render() {
-    return <div>hello</div>
+import ReactDOM from 'react-dom';
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
+    this.update = this.update.bind(this)
+  }
+  update(e){
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    })
+  }
+  render(){
+    return (
+      <div>
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
+        <br />
+      </div>
+    );
   }
 }
-//const App = () => <h1>hello stateless</h1>
 
-export default App
+class Slider extends React.Component {
+  render(){
+    return (
+        <div>
+        <input ref="inp" type="range"
+          min="0"
+          max="255"
+          onChange={this.props.update} />
+        </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
